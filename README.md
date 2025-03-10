dd9586550781933aacf34d842236dfcb044f22bc
holmesjj <hujiajun19961024@gmail.com>
2025-03-10 23:10:09 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Plot Figure
+
+## Quick Start
+
+### Python
+
+1. Install Anaconda following the [link](https://docs.anaconda.com/anaconda/install/index.html).
+
+2. Create and activate environment using the following commands.
+```
+# Create Python environment
+conda create --name plot-figure python=3.10.10
+
+# Check Python environment
+conda info --envs
+
+# Activate environment
+conda activate plot-figure
+
+# Deactivate environment
+conda deactivate
+
+# Remove environment
+conda remove -n plot-figure --all
+```
+
+3. Install `requirements.txt`.
+```
+pip install -r /path/to/requirements.txt
+```
---
+++ main.py
@@ -0,0 +1,72 @@
+import os
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from flask import Flask
+from flask import request
+from flask import send_from_directory
+from flask_cors import CORS
+from flask_restful import Api
+from flask_restful import Resource
+
+PLOT_DIR = "plots"
+
+application = Flask(__name__)
+application.config["CORS_HEADERS"] = "Content-Type"
+application.config["CORS_RESOURCES"] = {r"/api/*": {"origins": "*"}}
+application.config["PROPAGATE_EXCEPTIONS"] = True
+
+cors = CORS(application)
+api = Api(application)
+
+
+class Index(Resource):
+    def get(self):
+        return "Hello World"
+
+
+class Plot(Resource):
+    def post(self):
+        data = request.get_json()
+        response = {
+            "code": 0,
+            "message": ""
+        }
+        try:
+            plot_data = data.get("plot_description", {})
+            x_values = plot_data["x_axis"].get("values", [])
+            y_values = plot_data["y_axis"].get("values", [])
+            num_paths = len(y_values)
+            legend = plot_data.get("legend", [])
+            plt.figure(figsize=(10, 6))
+            for i in range(num_paths):
+                plt.plot(x_values, y_values[i], label=legend[i])
+            plt.legend(loc="best")
+            plt.title(plot_data["title"])
+            plt.xlabel(f"{plot_data['x_axis']['label']} ({plot_data['x_axis']['unit']})")
+            plt.ylabel(f"{plot_data['y_axis']['label']} ({plot_data['y_axis']['unit']})")
+            os.makedirs(PLOT_DIR, exist_ok=True)
+            file_name = "brownian_motion.png"
+            plot_path = os.path.join(PLOT_DIR, file_name)
+            plt.savefig(plot_path)
+            plt.close()
+            full_url = request.host_url + "plots/" + file_name
+            response["url"] = full_url
+        except Exception as e:
+            response["code"] = -1
+            response["message"] = str(e)
+        return response
+
+
+@application.route("/plots/<filename>")
+def get_plot(filename):
+    return send_from_directory(PLOT_DIR, filename)
+
+
+api.add_resource(Index, "/", endpoint="index")
+api.add_resource(Plot, "/api/plot", endpoint="plot")
+
+if __name__ == "__main__":
+    # application.debug = True
+    application.run(host="0.0.0.0", port=5050)
